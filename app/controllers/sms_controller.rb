0a8178d6d0c78218e2d3c904cb61833c21e7eafa
Berk Caputcu <berkcaputcu@gmail.com>
2013-07-30 18:01:05 -0400

    test

--- app/controllers/sms_controller.rb
+++ app/controllers/sms_controller.rb
@@ -3,7 +3,9 @@ class SmsController < ApplicationController
 		message_body = params["Body"]
 		from_number = params["From"]
 
+		puts message_body
+
 		Item.create!(name: message_body)
-		
+
 	end
 end
