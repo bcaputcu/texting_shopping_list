628fe0c24e007ab55cbcedfac010cd08a1cc4f0c
Berk Caputcu <berkcaputcu@gmail.com>
2013-07-30 19:45:11 -0400

    send recieve sms

--- app/controllers/sms_controller.rb
+++ app/controllers/sms_controller.rb
@@ -3,9 +3,28 @@ class SmsController < ApplicationController
 		message_body = params["Body"]
 		from_number = params["From"]
 
-		puts message_body
+		current_list_string = Item.current_list.join(',')
 
-		Item.create!(name: message_body)
+		if message_body.blank?
+			to_send = current_list_string
+		else
+			item = Item.create(name: message_body)
 
+			if item.valid?
+				to_send = "#{item.name} added. Current list: #{current_list_string}".truncate(160)
+			else
+				to_send = "Error! Report to Berk Caputcu!"
+			end
+		end
+
+		@twilio_client = Twilio::REST::Client.new ENV['twilio_sid'], ENV['twilio_token']
+
+		@twilio_client.account.sms.messages.create(
+			:from => "+1#{ENV['twilio_phone_number']}",
+			:to => from_number,
+			:body => to_send
+			)
+
+		render :json => { :status => :ok }
 	end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,10 @@
 class Item < ActiveRecord::Base
   attr_accessible :done, :name
+
+  validates_presence_of :name
+
+  def self.current_list
+  	Item.where done: false
+  end
+
 end
